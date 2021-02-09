9c51ab9c892bc9ddae295a7516577ee138ee51b3
ashleysm <75022931+ashleysm@users.noreply.github.com>
2021-02-09 12:40:43 +0530

    Add files via upload

---
+++ Design Principles/hands on/hands on 2/Automobile/MercedesFactory.java
@@ -0,0 +1,13 @@
+package Automobiles;
+
+public class MercedesFactory extends Factory{
+
+	@Override
+	public Headlight makeHeadlight() {
+		return new MercedesHeadlight();
+	}
+
+	@Override
+	public Tire makeTire() {
+		return new MercedesTire();
+	}
